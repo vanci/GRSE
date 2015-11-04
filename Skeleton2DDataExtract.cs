badfa107ebc449cb3fbe33154bd585df76b5f010
vanci <vanchi.lee.7@gmail.com>
2015-11-04 13:58:02 -0500

    added DTWGestureRecognition to the KinectViewer sample code
    Not ready for compile

---
+++ Skeleton2DDataExtract.cs
@@ -0,0 +1,86 @@
+﻿namespace DTWGestureRecognition
+{
+    using System;
+    using System.Windows;
+    using Microsoft.Kinect;
+
+    /// <summary>
+    /// This class is used to transform the data of the skeleton
+    /// </summary>
+    internal class Skeleton2DDataExtract
+    {
+        /// <summary>
+        /// Skeleton2DdataCoordEventHandler delegate
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="a">Skeleton 2Ddata Coord Event Args</param>
+        public delegate void Skeleton2DdataCoordEventHandler(object sender, Skeleton2DdataCoordEventArgs a);
+
+        /// <summary>
+        /// The Skeleton 2Ddata Coord Ready event
+        /// </summary>
+        public static event Skeleton2DdataCoordEventHandler Skeleton2DdataCoordReady;
+
+        /// <summary>
+        /// Crunches Kinect SDK's Skeleton Data and spits out a format more useful for DTW
+        /// </summary>
+        /// <param name="data">Kinect SDK's Skeleton Data</param>
+        public static void ProcessData(Skeleton data)
+        {
+            // Extract the coordinates of the points.
+            var p = new Point[6];
+            Point shoulderRight = new Point(), shoulderLeft = new Point();
+            if (data != null)
+            {
+                foreach (Joint j in data.Joints)
+                {
+                    switch (j.JointType)
+                    {
+                        case JointType.HandLeft:
+                            p[0] = new Point(j.Position.X, j.Position.Y);
+                            break;
+                        case JointType.WristLeft:
+                            p[1] = new Point(j.Position.X, j.Position.Y);
+                            break;
+                        case JointType.ElbowLeft:
+                            p[2] = new Point(j.Position.X, j.Position.Y);
+                            break;
+                        case JointType.ElbowRight:
+                            p[3] = new Point(j.Position.X, j.Position.Y);
+                            break;
+                        case JointType.WristRight:
+                            p[4] = new Point(j.Position.X, j.Position.Y);
+                            break;
+                        case JointType.HandRight:
+                            p[5] = new Point(j.Position.X, j.Position.Y);
+                            break;
+                        case JointType.ShoulderLeft:
+                            shoulderLeft = new Point(j.Position.X, j.Position.Y);
+                            break;
+                        case JointType.ShoulderRight:
+                            shoulderRight = new Point(j.Position.X, j.Position.Y);
+                            break;
+                    }
+                }
+                // Centre the data
+                var center = new Point((shoulderLeft.X + shoulderRight.X) / 2, (shoulderLeft.Y + shoulderRight.Y) / 2);
+                for (int i = 0; i < 6; i++)
+                {
+                    p[i].X -= center.X;
+                    p[i].Y -= center.Y;
+                }
+                // Normalization of the coordinates
+                double shoulderDist =
+                    Math.Sqrt(Math.Pow((shoulderLeft.X - shoulderRight.X), 2) +
+                              Math.Pow((shoulderLeft.Y - shoulderRight.Y), 2));
+                for (int i = 0; i < 6; i++)
+                {
+                    p[i].X /= shoulderDist;
+                    p[i].Y /= shoulderDist;
+                }
+                // Launch the event!
+                Skeleton2DdataCoordReady(null, new Skeleton2DdataCoordEventArgs(p));
+            }
+        }
+    }
+}
---
+++ Skeleton2DdataCoordEventArgs.cs
@@ -0,0 +1,50 @@
+﻿namespace DTWGestureRecognition
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Takes Kinect SDK Skeletal Frame coordinates and converts them intoo a format useful to th DTW
+    /// </summary>
+    internal class Skeleton2DdataCoordEventArgs
+    {
+        /// <summary>
+        /// Positions of the elbows, the wrists and the hands (placed from left to right)
+        /// </summary>
+        private readonly Point[] _points;
+
+        /// <summary>
+        /// Initializes a new instance of the Skeleton2DdataCoordEventArgs class
+        /// </summary>
+        /// <param name="points">The points we need to handle in this class</param>
+        public Skeleton2DdataCoordEventArgs(Point[] points)
+        {
+            _points = (Point[]) points.Clone();
+        }
+
+        /// <summary>
+        /// Gets the point at a certain index
+        /// </summary>
+        /// <param name="index">The index we wish to retrieve</param>
+        /// <returns>The point at the sent index</returns>
+        public Point GetPoint(int index)
+        {
+            return _points[index];
+        }
+
+        /// <summary>
+        /// Gets the coordinates of our _points
+        /// </summary>
+        /// <returns>The coordinates of our _points</returns>
+        internal double[] GetCoords()
+        {
+            var tmp = new double[_points.Length * 2];
+            for (int i = 0; i < _points.Length; i++)
+            {
+                tmp[2 * i] = _points[i].X;
+                tmp[(2 * i) + 1] = _points[i].Y;
+            }
+
+            return tmp;
+        }
+    }
+}
